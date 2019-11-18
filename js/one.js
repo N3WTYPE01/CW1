b12363741534d8e7ff1e161f84a0e26a90e85614
Zainul Vali <zainul@example.com>
2019-11-18 23:09:05 +0000

    code for log in validation and page 2

--- js/one.js
+++ js/one.js
@@ -1,38 +1,62 @@
-Vue.component('myheading',{
-  template: '<h1>School Activity System</h1>' 
-});
-
-
-new Vue({
-    el: "#app"
-    });
-
-    var signupApp = new Vue({
-        el: '#signup',
-        data: {
-        email: '',
-        username:'',
-        password: '',
-        },
-        methods: {
-        submitForm: function() {var users = '';
-        var newEmail = this.email;
-        if (localStorage.getItem('users')) { // 'users' is an array of objects
+var vm = new Vue({
+  el: '#register',
+  data: {
+    email: '',
+    username: '',
+    password: '',
+  },
+  // define methods under the methods object
+  methods: {
+    submitRegister: function () {
+      var users = '';
+      var newEmail = this.email;
+      if (localStorage.getItem('users')) { // 'users' is an array of objects
         users = JSON.parse(localStorage.getItem('users'));
-        };
-        if (users) {
-        if (users.some(function(user) {return user.email === newEmail})) {
-        alert('Email already exists!');
+      };
+      if (users) {
+        if (users.some(function (user) { return user.email === newEmail })) {
+          alert('Email already exists!');
+          return;
+        }
+        users.push({ 'email': newEmail, 'username': this.username, 'password': this.password });
+        localStorage.setItem('users', JSON.stringify(users));
+      }
+      else {
+        users = [{ 'email': newEmail, 'username': this.username, 'password': this.password }];
+        localStorage.setItem('users', JSON.stringify(users));
         return;
+      }
+    }
+  }
+});
+//Vue instance for log in form
+var vm = new Vue({
+    el: '#login',
+    data: {
+        loginUsername: '',
+        loginPassword: '',
+    },
+    methods: {// function for log in
+    log: function () {
+      var users = '';
+      var logUsername = this.loginUsername;
+      var logPasseord = this.loginPassword;
+      if (localStorage.getItem('users')) { // 'users' is an array of objects
+        users = JSON.parse(localStorage.getItem('users'));
+      };
+      if (users) { //user validation if username and password matches localstorage
+        if (users.some(function (user) { return user.username === logUsername, user.password === logPasseord})) {
+          confirm('User ' +  logUsername +  ' Logged In!');
+          window.location.href = 'page2.html', true;
+          return;
         }
-        users.push({'email': newEmail,'username': this.username,'password': this.password});
-        localStorage.setItem('users', JSON.stringify(users));
-        }
-        else {
-        users = [{'email': newEmail, 'username':this.username, 'password': this.password}];
-        localStorage.setItem('users', JSON.stringify(users));
-        }
-        }
-        }
-        } 
-    )
+      }
+      else (users) 
+        if (users.some(function (user) { return user.username !== logUsername })) {
+          alert('Incorrect Username or Password. Please try again.');
+      }
+    }
+    }
+}
+)
+
